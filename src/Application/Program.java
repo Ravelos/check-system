708d7ba734650d4d2e22ae6747efa9eefdbf089f
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-07-05 18:32:01 -0300

    First commit

---
+++ src/Application/Program.java
@@ -0,0 +1,10 @@
+package Application;
+
+import boardgame.Board;
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args){
+        Board board = new Board(8,8);
+    }
+}

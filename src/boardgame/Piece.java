015bb5ca4ea1e4e657520f259b07d0e1b1b95576
Oswaldo Castillo <raveloswaldo@gmail.com>
2024-10-15 22:15:56 -0300

    Creates the method to print the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -4,12 +4,12 @@ public class Piece {
     protected Position position;
     private Board board;
 
-    protected Board getBoard() {
-        return board;
-    }
-
     public Piece(Board board) {
         this.board = board;
         position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
     }
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,8 +7,14 @@ public class ChessMatch {
     public ChessMatch(){
         board = new Board(8,8);
     }
-    public ChessPiece[][] getPieces(){
+    public ChessPiece[][] getPieces() {
         ChessPiece [][] match = new ChessPiece[board.getRows()][board.getColumns()];
+        for (int i=0; i < board.getRows(); i++) {
+            for (int j=0; j < board.getColumns(); j++) {
+                match[i][j] = (ChessPiece) board.piece(i,j);
+            }
+        }
+        return match;
     }
 
 }

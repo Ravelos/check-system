34e8338097c71d576dc2513a3cd3f549301442e7
Oswaldo Castillo <raveloswaldo@gmail.com>
2024-10-16 16:32:13 -0300

    Fixes issues with the board printed

--- src/application/UI.java
+++ src/application/UI.java
@@ -6,8 +6,8 @@ public class UI {
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i=0; i<pieces.length; i++) {
-            System.out.println((8-1) + " ");
-            for (int j=0; j< pieces.length; j++) {
+            System.out.print((8-i) + " ");
+            for (int j=0; j< pieces[i].length; j++) {
                 printPiece(pieces[i][j]);
             }
             System.out.println();
@@ -18,10 +18,10 @@ public class UI {
 
     public static void printPiece(ChessPiece piece) {
         if (piece == null) {
-            System.out.println("-");
+            System.out.print("-");
         }else {
-            System.out.println(piece);
+            System.out.print(piece);
         }
-        System.out.println(" ");
+        System.out.print(" ");
     }
 }

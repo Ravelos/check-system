4c1acd33a8cf9c242872b66834984b484d7b2a73
Oswaldo Castillo <raveloswaldo@gmail.com>
2024-10-16 17:02:29 -0300

    Creates king and rook pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,11 +1,16 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
+
     public ChessMatch(){
         board = new Board(8,8);
+        initialSetUp();
     }
     public ChessPiece[][] getPieces() {
         ChessPiece [][] match = new ChessPiece[board.getRows()][board.getColumns()];
@@ -15,6 +20,12 @@ public class ChessMatch {
             }
         }
         return match;
+    }
+
+    private void initialSetUp() {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+        board.placePiece(new King(board, Color.BLACK), new Position(0,4));
+        board.placePiece(new King(board, Color.WHITE), new Position(7,4));
     }
 
 }
